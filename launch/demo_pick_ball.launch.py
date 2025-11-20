7296115f2436a955906abbd797cb185412420305
clara-summerford <clara.summerford@my.utexas.edu>
2025-11-20 06:52:30 -0600

    working pick and place

---
+++ launch/demo_pick_ball.launch.py
@@ -0,0 +1,60 @@
+from launch import LaunchDescription
+from launch.actions import GroupAction
+from launch_ros.actions import SetRemap, SetParameter, PushRosNamespace, Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_move_group_launch
+from spot_description.get_accessories import get_accessories_from_env    
+from ament_index_python.packages import get_package_share_directory
+import os
+    
+def generate_launch_description():
+
+    # Launch args
+    launch_args = [
+        DeclareLaunchArgument('kinematic_model',
+                            description='The kinematic model to use for the Spot description',
+                            choices=['none', 'body_assist', 'mobile_manipulation'],
+                            default_value='none')
+    ]
+
+    xacro_args = get_accessories_from_env()
+    xacro_args['kinematic_model'] = LaunchConfiguration('kinematic_model')
+    moveit_config_builder = MoveItConfigsBuilder("spot", package_name="spot_moveit_config")
+    moveit_config_builder.robot_description(mappings=xacro_args)
+    moveit_config_builder.robot_description_semantic(mappings=xacro_args)
+    moveit_config = moveit_config_builder.to_moveit_configs()
+
+    return LaunchDescription([
+        *launch_args,
+        GroupAction(
+            actions=[
+                PushRosNamespace('spot_moveit'), #prev commented
+                SetRemap(src='joint_states', dst='/spot_driver/joint_states'),
+                SetRemap(src='robot_description', dst='/spot_driver/robot_description'), #prev commented
+                # SetParameter(name="octomap_resolution", value=0.03),
+                # SetParameter(name="octomap_frame", value="spot_nav/map"),
+                Node(
+                    package='spot_arm_control',
+                    executable='demo_pick_ball',
+                    # namespace='spot_moveit',
+                    output='screen',
+                    # remappings=[('/joint_states', '/spot_driver/joint_states')],
+                    parameters=[
+                        moveit_config.robot_description,
+                        moveit_config.robot_description_semantic,
+                        moveit_config.robot_description_kinematics,
+                    ],
+                )
+            ]
+        )
+    ])
+
+
+
+
+
+
+
+
